00270deada07fd20d2735ac029b862256f97aa41
DanielSierra17 <dfsierra898@misena.edu.co>
2022-06-20 15:11:09 -0500

    Diseño Catalogo Mejorado

--- resources/views/productos/categoria.blade.php
+++ resources/views/productos/categoria.blade.php
@@ -10,7 +10,7 @@
 <div class="row" style="display: inline-block">
     <div class="col">  
         <div class="card">
-            <div class="card-image" style="height:150px; width:200px">
+            <div class="card-image" style="height:300px; width:300px">
                 @if( $producto->imagen === null )
                     <img src="{{ asset( 'img/producto_no_disponible.png' ) }}" alt="" />
     
@@ -19,13 +19,13 @@
 
                 @endif
                 
-                <span class="card-title">{{ $producto->nombre }}</span>
+                <span class="card-title cyan-text text-darken-3">{{ $producto->nombre }}</span>
             </div>
             <div class="card-content">
                 <p>{{ $producto->desc }}</p>
             </div>
             <div class="card-action">
-            <a href="{{route ('productos.show', $producto->id)}}">Ver Detalles</a>
+            <a href="{{route ('productos.show', $producto->id)}}" class="cyan-text text-darken-3"">Ver Detalles</a>
             </div>
         </div>
     </div>
